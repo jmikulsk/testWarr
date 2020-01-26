9bd1b5ed64d7459251f320cfd6db519e2a7ff52b
jmikulsk <jmikulsk@wszib.edu.pl>
2020-01-26 09:12:41 +0100

    Initial commit

---
+++ src/main/java/com/example/testWarr/TestWarrApplication.java
@@ -0,0 +1,13 @@
+package com.example.testWarr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWarrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestWarrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testWarr/TestWarrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testWarr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWarrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
